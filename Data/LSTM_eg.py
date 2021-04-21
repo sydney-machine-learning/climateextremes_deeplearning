24aa8c2b666c153b80d505a882753f9345ed64a5
RC <c.rohitash@gmail.com>
2021-04-21 14:37:25 +1000

    update

--- Data/LSTM_eg.py
+++ Data/LSTM_eg.py
@@ -24,119 +24,173 @@ from numpy import array
 
 # split a univariate sequence into samples
 def split_sequence(sequence, n_steps_in, n_steps_out):
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        # find the end of this pattern
-        end_ix = i + n_steps_in
-        out_end_ix = end_ix + n_steps_out
-        # check if we are beyond the sequence
-        if out_end_ix > len(sequence):
-            break
-        # gather input and output parts of the pattern
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return np.array(X), np.array(y)
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps_in
+		out_end_ix = end_ix + n_steps_out
+		# check if we are beyond the sequence
+		if out_end_ix > len(sequence):
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
 
 
 
 
 def rmse(pred, actual):
-    return np.sqrt(((pred - actual) ** 2).mean())
+	return np.sqrt(((pred - actual) ** 2).mean())
 
 
 # define the model
 
 def MODEL_LSTM(name, x_train, x_test, y_train, y_test, Num_Exp, n_steps_in, n_steps_out, Epochs, Hidden):
-    n_features = 1
-    x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], n_features))
-    print(x_train.shape)
-    x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], n_features))
-    print(x_test.shape)
+	n_features = 1
+	x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], n_features))
+	print(x_train.shape)
+	x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], n_features))
+	print(x_test.shape)
 
-    train_acc = np.zeros(Num_Exp)
-    test_acc = np.zeros(Num_Exp)
-    Step_RMSE = np.zeros([Num_Exp, n_steps_out])
+	train_acc = np.zeros(Num_Exp)
+	test_acc = np.zeros(Num_Exp)
+	Step_RMSE = np.zeros([Num_Exp, n_steps_out])
 
-    model = Sequential()
-    model.add(LSTM(Hidden, activation='relu', input_shape=(n_steps_in, n_features), dropout=0.2))
-    model.add(Dense(32))
-    model.add(Dense(n_steps_out))
-    model.compile(optimizer='adam', loss='mse')
-    model.summary()
-    future_prediction = np.zeros([Num_Exp, 60])
-    Best_RMSE = 1000  # Assigning a large number
+	model = Sequential()
+	model.add(LSTM(Hidden, activation='relu', input_shape=(n_steps_in, n_features), dropout=0.2))
+	model.add(Dense(32))
+	model.add(Dense(n_steps_out))
+	model.compile(optimizer='adam', loss='mse')
+	model.summary()
+	future_prediction = np.zeros([Num_Exp, 60])
+	Best_RMSE = 1000  # Assigning a large number
 
-    start_time = time.time()
-    for run in range(Num_Exp):
-        print("Experiment", run + 1, "in progress")
-        # fit model
-        model.fit(x_train, y_train, epochs=Epochs, batch_size=10, verbose=0, shuffle=False)
+	start_time = time.time()
+	for run in range(Num_Exp):
+		print("Experiment", run + 1, "in progress")
+		# fit model
+		model.fit(x_train, y_train, epochs=Epochs, batch_size=10, verbose=0, shuffle=False)
 
-        y_predicttrain = model.predict(x_train)
-        y_predicttest = model.predict(x_test)
-        # print(y_predicttest)
-        train_acc[run] = rmse(y_predicttrain, y_train)
-        test_acc[run] = rmse(y_predicttest, y_test)
-        if test_acc[run] < Best_RMSE:
-            Best_RMSE = test_acc[run]
-            Best_Predict_Test = y_predicttest
-        for j in range(n_steps_out):
-            Step_RMSE[run][j] = rmse(y_predicttest[:, j], y_test[:, j])
+		y_predicttrain = model.predict(x_train)
+		y_predicttest = model.predict(x_test)
+		# print(y_predicttest)
+		train_acc[run] = rmse(y_predicttrain, y_train)
+		test_acc[run] = rmse(y_predicttest, y_test)
+		if test_acc[run] < Best_RMSE:
+			Best_RMSE = test_acc[run]
+			Best_Predict_Test = y_predicttest
+		for j in range(n_steps_out):
+			Step_RMSE[run][j] = rmse(y_predicttest[:, j], y_test[:, j])
 
-        #chain_inp = []
-        #chain_out = []
-        #chain_inp.append(list(future_predict_df.tail(1).iloc[0, 0:6]))
-        #chain_out.append(list(future_predict_df.tail(1).iloc[0, 6:10]))
-        #chain_inp = np.asarray(chain_inp, dtype=np.float32)
-        #chain_out = np.asarray(chain_out, dtype=np.float32)
-        #results = []
-        # for step in range (1,16):
-        # chain_inp = np.concatenate([chain_inp.reshape(chain_inp.shape[0],chain_inp.shape[1],n_features)[:,-2:,:],chain_out.reshape(chain_out.shape[0],chain_out.shape[1],n_features)],axis=1)
-        # chain_out = model.predict(chain_inp)
-        # print(chain_out.shape)
-        # for pred in chain_out[0]:
-        # results.append(pred)
-        # future_prediction[run][:] = np.ndarray.flatten(scaler.inverse_transform(np.reshape(results,(len(results),1))))
-        # print(future_prediction)
-    print("Total time for", Num_Exp, "experiments", time.time() - start_time)
-    return future_prediction, train_acc, test_acc, Step_RMSE, Best_Predict_Test, y_predicttrain, y_predicttest
+		#chain_inp = []
+		#chain_out = []
+		#chain_inp.append(list(future_predict_df.tail(1).iloc[0, 0:6]))
+		#chain_out.append(list(future_predict_df.tail(1).iloc[0, 6:10]))
+		#chain_inp = np.asarray(chain_inp, dtype=np.float32)
+		#chain_out = np.asarray(chain_out, dtype=np.float32)
+		#results = []
+		# for step in range (1,16):
+		# chain_inp = np.concatenate([chain_inp.reshape(chain_inp.shape[0],chain_inp.shape[1],n_features)[:,-2:,:],chain_out.reshape(chain_out.shape[0],chain_out.shape[1],n_features)],axis=1)
+		# chain_out = model.predict(chain_inp)
+		# print(chain_out.shape)
+		# for pred in chain_out[0]:
+		# results.append(pred)
+		# future_prediction[run][:] = np.ndarray.flatten(scaler.inverse_transform(np.reshape(results,(len(results),1))))
+		# print(future_prediction)
+	print("Total time for", Num_Exp, "experiments", time.time() - start_time)
+	return future_prediction, train_acc, test_acc, Step_RMSE, Best_Predict_Test, y_predicttrain, y_predicttest
+
+def edi_cat(values):
+	
+	category = np.zeros(values.shape[0])
+	
+	print(category)
+	
+	for i in range(0, values.shape[0]):
+		if values[i] >= 2: 
+			category[i] = 6
+			
+		elif values[i] > 1.5 and values[i] <= 1.99: 
+			category[i] = 5
+			
+		elif values[i] > 1.0 and values[i] <= 1.49: 
+			category[i] = 4
+			
+		elif values[i] > -0.99 and values[i] <= 0.99: 
+			category[i] = 3
+			
+		elif values[i] > -1.49 and values[i] <= -1.0: 
+			category[i] = 2
+			
+		elif values[i] > -1.99 and values[i] <= -1.5: 
+			category[i] = 1
+			
+		elif values[i] <= -2: 
+			category[i] = 0
+		else:
+			category[i] = 7
+	print(category)
+	print(values)
+
+	return  category 
 
 
 #------------------
 
-num_grids = 22
+num_grids = 2
 
 for grid in range(num_grids):
 
-    print(grid, ' is grid')
+	print(grid, ' is grid')
 
-    data = np.genfromtxt("Edi_month_grid - Copy.csv", delimiter=',')
-    univariate_g1 = data[:, grid]
-    train = univariate_g1[0:420]
-    test = univariate_g1[421:457]
-
-
-    n_steps_in = 3
-    n_steps_out = 3
-    x_train, y_train = split_sequence(train, n_steps_in, n_steps_out)
-
-    # summarize the data 
-     
-    x_test, y_test = split_sequence(test, n_steps_in, n_steps_out)
-     
+	data = np.genfromtxt("Edi_month_grid - Copy.csv", delimiter=',')
+	univariate_g1 = data[:, grid]
+	train = univariate_g1[0:420]
+	test = univariate_g1[421:457]
 
 
-    Hidden = 10
-    Epochs = 20
-    n_steps_out = 3
-    n_steps_in = 3
-    name = 'Grid1'
-    Num_Exp = 2
+	n_steps_in = 3
+	n_steps_out = 3
+	x_train, y_train = split_sequence(train, n_steps_in, n_steps_out)
 
-    future_prediction, train_acc, test_acc, Step_RMSE, Best_Predict_Test, y_predicttrain, y_predicttest = MODEL_LSTM(name,x_train,x_test,y_train,y_test,Num_Exp,n_steps_in,n_steps_out,Epochs, Hidden)
+	# summarize the data 
+	 
+	x_test, y_test = split_sequence(test, n_steps_in, n_steps_out)
+	 
 
-    print(train_acc, test_acc) 
+
+	Hidden = 10
+	Epochs = 20
+	n_steps_out = 3
+	n_steps_in = 3
+	name = 'Grid1'
+	Num_Exp = 5
+
+	future_prediction, train_acc, test_acc, Step_RMSE, Best_Predict_Test, y_predicttrain, y_predicttest = MODEL_LSTM(name,x_train,x_test,y_train,y_test,Num_Exp,n_steps_in,n_steps_out,Epochs, Hidden)
+
+	
+
+	print(train_acc, test_acc) 
+
+	mean_train = np.mean(train_acc, axis=1)
+
+	mean_test = np.std(test_acc, axis=1)
+
+
+	print(mean_train, mean_test) 
+
+
+	values = y_predicttest[:,0]
+
+
+	category = edi_cat(values)
+
+	print(category, ' categories')
+
+
+	np.savetxt(values, 'values.txt')
 
 #np.mean
 # confidence interval from std
